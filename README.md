2574cfb8a55de23c0ad567aace59904be1c4b732
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2017-03-24 13:51:34 +0000

    first working app reading loadavg1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # databox-app-simple1
 
-A simple Databox app in Node.js
+A simple Databox app in Node.js.
+
+Display the loadavg1 from the os monitor driver.
+
--- server.js
+++ server.js
@@ -1,3 +1,53 @@
-var databox = require('node-databox');
+const https = require('https');
+const url = require('url');
 
-console.log('hello');
+const express = require('express');
+const bodyParser = require('body-parser');
+//const WebSocket = require('ws');
+const databox = require('node-databox');
+
+const DATASOURCE_loadavg1 = JSON.parse(process.env.DATASOURCE_loadavg1 || '{}');
+var loadavg1_ENDPOINT = DATASOURCE_loadavg1.href || '';
+
+console.log('datasource: '+loadavg1_ENDPOINT);
+
+// for API & UI web server
+const PORT = process.env.PORT || 8080;
+
+const HTTPS_SERVER_CERT = process.env.HTTPS_SERVER_CERT || '';
+const HTTPS_SERVER_PRIVATE_KEY = process.env.HTTPS_SERVER_PRIVATE_KEY || '';
+const credentials = {
+	key:  HTTPS_SERVER_PRIVATE_KEY,
+	cert: HTTPS_SERVER_CERT,
+};
+
+const app = express();
+
+app.get('/status', function(req, res){
+	res.send('active');
+});
+
+app.get('/ui', function(req, res){
+	// this is stupid!
+	var endpointUrl = url.parse(loadavg1_ENDPOINT);
+	var dsUrl = endpointUrl.protocol + '//' + endpointUrl.host;
+	var dsName = endpointUrl.path.replace('/','');
+	// how do i know it is a time-series?
+	databox.timeseries.latest(dsUrl, dsName)
+	.then((data)=>{
+        console.log('Got data '+JSON.stringify(data));
+        if (data.length>0) {
+        	res.send('loadavg1 = '+data[0].data);
+        }
+        else {
+        	res.send('No data');
+        }
+    })
+    .catch((err)=>{
+        console.log("[Error getting timeseries.latest]",dsUrl, dsID);
+        res.send('Error getting data');
+    });
+});
+
+//start the express server
+https.createServer(credentials, app).listen(PORT);
